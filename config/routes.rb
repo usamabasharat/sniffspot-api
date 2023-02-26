cdd370a0f055f17637b1557e79d5baae5f1c50b6
Usama Basharat <dev.usamabasharat@gmail.com>
2023-02-26 05:19:33 +0500

    Added default host

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  default_url_options host: 'localhost:3000' if Rails.env.test?
+  default_url_options host: 'https://sniffspot-api.fly.dev'
+
   namespace :api do
     resources :spots, only: %i[index create update] do
       resources :reviews, shallow: true, only: %i[create update]
