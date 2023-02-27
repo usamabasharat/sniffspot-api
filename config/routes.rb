92a8fc8f90599531971e53164757ac058d9e8cb2
Usama Basharat <dev.usamabasharat@gmail.com>
2023-02-27 15:05:54 +0500

    Dockerized the app

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@
 
 Rails.application.routes.draw do
   default_url_options host: 'localhost:3000' if Rails.env.test?
-  default_url_options host: 'https://sniffspot-api.fly.dev'
+  default_url_options host: 'https://illusive-app.herokuapp.com'
 
   namespace :api do
     resources :spots, only: %i[index create update] do
